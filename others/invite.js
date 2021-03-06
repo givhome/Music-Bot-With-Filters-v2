32f38262c80653fceaa447085612ab459850b4e2
givhome <79974727+givhome@users.noreply.github.com>
2021-03-06 03:29:41 +0300

    Update invite.js

--- others/invite.js
+++ others/invite.js
@@ -10,7 +10,7 @@ module.exports = {
       .setTitle("Add us to your server!")
       .setDescription("Love using Harmony? Great, Thank you! Consider adding it to your server")
       .setColor("#F0EAD6")
-      .setAuthor('Harmony','https://cdn.discordapp.com/attachments/778600026280558617/781024479623118878/ezgif.com-gif-maker_1.gif')
+      .setAuthor('Harmony','https://media.discordapp.net/attachments/810195648422477885/817474978835857429/image0.gif')
       .setThumbnail(message.guild.iconURL())
       .addField(`Use the following link to add Harmony to your discord server`, 'https://discord.com/api/oauth2/authorize?client_id=767885987740254291&permissions=49572160&scope=bot', true)
 
